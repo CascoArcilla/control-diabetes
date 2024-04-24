bbc4aefd93a46a50936c1d6a80f0d8a3e1b7777b
CascoArcilla <normon1230@gmail.com>
2024-04-24 12:21:07 -0600

    Agredado favicon y modificado la ruta root

---
+++ src/components/header/Hedaer.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="container-lg pt-2 border">
+      <div className="bg-body-tertiary">
+        <div className="container-fluid d-flex">
+          <h2>DiabeTics</h2>
+          <p>SAJDH</p>
+        </div>
+      </div>
+    </header>
+  );
+}
