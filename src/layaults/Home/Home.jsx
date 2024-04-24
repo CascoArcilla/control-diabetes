3b66e41df5b08257fb9d49e48d1d52b8742b187d
CascoArcilla <normon1230@gmail.com>
2024-04-23 18:53:10 -0600

    Otra prueba en src/layaults/Home/Home.jsx

--- src/layaults/Home/Home.jsx
+++ src/layaults/Home/Home.jsx
@@ -1,42 +1,15 @@
 export default function Home() {
   return (
-    <div className="container-sm d-flex justify-content-center align-items-center flex-column gap-3">
-      <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            Card subtitle
-          </h6>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
-        </div>
-      </div>
-      <div className="card" style={{ width: "18rem" }}>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            Card subtitle
-          </h6>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
-        </div>
-      </div>
-    </div>
+    <>
+      <h1 className="text-center">Welcome to DiabeTics</h1>
+      <figure className="text-center">
+        <blockquote className="blockquote">
+          <p>Una app web para llevar un control de la dibates</p>
+        </blockquote>
+        <figcaption className="blockquote-footer">
+          Hecho por <cite title="Source Title">Alumnos</cite>
+        </figcaption>
+      </figure>
+    </>
   );
 }
