551d1b9b4d2de3af83bda1bd6a52260425ce92f6
CascoArcilla <normon1230@gmail.com>
2024-04-23 18:45:45 -0600

    Intalado Bootstrap y probado en src/layaults/Home/Home.jsx

--- src/layaults/Home/Home.jsx
+++ src/layaults/Home/Home.jsx
@@ -1,12 +1,42 @@
-import { useParams } from "react-router-dom";
-
 export default function Home() {
-  const { iduser } = useParams();
-
   return (
-    <>
-      <h1>Welcome</h1>
-      {iduser ? <p>{iduser}</p> : ""}
-    </>
+    <div className="container-sm d-flex justify-content-center align-items-center flex-column gap-3">
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            Card subtitle
+          </h6>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the cards content.
+          </p>
+          <a href="#" className="card-link">
+            Card link
+          </a>
+          <a href="#" className="card-link">
+            Another link
+          </a>
+        </div>
+      </div>
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
+            Card subtitle
+          </h6>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the cards content.
+          </p>
+          <a href="#" className="card-link">
+            Card link
+          </a>
+          <a href="#" className="card-link">
+            Another link
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
