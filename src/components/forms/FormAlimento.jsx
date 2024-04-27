137dbc42c5652d56b5e3839c8b8e839fe6e30dd6
CascoArcilla <normon1230@gmail.com>
2024-04-27 01:27:41 -0600

    Se agrea funcion de guardar alimento en localstorage

--- src/components/forms/FormAlimento.jsx
+++ src/components/forms/FormAlimento.jsx
@@ -1,69 +1,108 @@
-import { Form } from "react-router-dom";
+import { Form, Navigate, useActionData } from "react-router-dom";
+import { createAlimento } from "../../storage/alimentos";
+import { useAuth } from "../../auth/AuthPorvider";
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const dataAlimento = Object.fromEntries(formData);
+
+  const newDataAlimeto = await createAlimento(dataAlimento);
+
+  if (!newDataAlimeto) {
+    return { message: "Ah ocurrido un error", state: false };
+  }
+
+  return { message: "ok", state: true };
+}
 
 export default function FormAlimento() {
-    return (
-        <Form>
-            <h2>Registra alimento</h2>
-            <div className="container-fluid p-0 ">
-                <label htmlFor="input-alimento" className="form-label fw-bold">
-                    Nombre Alimento
-                </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="input-alimento"
-                    name="username"
-                    required
-                />
-            </div>
-            <div className="container-fluid p-0 ">
-                <label htmlFor="input-alimento" className="form-label fw-bold">
-                    Carbohidratos 
-                </label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="input-alimento"
-                    name="username"
-                    required
-                />
-            </div>
-            <div className="container-fluid p-0 ">
-                <label htmlFor="input-alimento" className="form-label fw-bold">
-                    Grasas 
-                </label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="input-alimento"
-                    name="username"
-                    required
-                />
-            </div>
-            <div className="container-fluid p-0 ">
-                <label htmlFor="input-alimento" className="form-label fw-bold">
-                    Calorias 
-                </label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="input-alimento"
-                    name="username"
-                    required
-                />
-            </div>
-            <div className="container-fluid p-0 ">
-                <label htmlFor="input-alimento" className="form-label fw-bold">
-                    Azucares 
-                </label>
-                <input
-                    type="number"
-                    className="form-control"
-                    id="input-alimento"
-                    name="username"
-                    required
-                />
-            </div>
-        </Form>
-    );
+  const action = useActionData();
+  const { user } = useAuth();
+
+  if (action) {
+    if (action.message == "ok" && action.state) {
+      return <Navigate to="/" />;
+    }
+  }
+
+  return (
+    <Form
+      method="post"
+      className="bg-body-secondary rounded  py-3 px-3  container-sm container-max-400 d-flex flex-column gap-3  align-items-center justify-content-center"
+    >
+      <h2>Registra alimento</h2>
+      <div className="container-fluid p-0 ">
+        <label htmlFor="input-alimento" className="form-label fw-bold m-0 p-0 ">
+          Nombre Alimento
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="input-alimento"
+          name="alimento"
+          required
+        />
+      </div>
+      <div className="container-fluid p-0 ">
+        <label htmlFor="input-carbo" className="form-label fw-bold m-0 p-0 ">
+          Carbohidratos (g)
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="input-carbo"
+          name="carbohidrato"
+          required
+        />
+      </div>
+      <div className="container-fluid p-0 ">
+        <label htmlFor="input-grasas" className="form-label fw-bold m-0 p-0 ">
+          Grasas (g)
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="input-grasas"
+          name="grasas"
+          required
+        />
+      </div>
+      <div className="container-fluid p-0 ">
+        <label htmlFor="input-calorias" className="form-label fw-bold m-0 p-0 ">
+          Calorias (kcal)
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="input-calorias"
+          name="calorias"
+          required
+        />
+      </div>
+      <div className="container-fluid p-0 ">
+        <label htmlFor="input-azucar" className="form-label fw-bold m-0 p-0 ">
+          Azucares (g)
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="input-azucar"
+          name="azucar"
+          required
+        />
+      </div>
+      <input
+        className="d-none"
+        type="text"
+        value={user.id}
+        name="iduser"
+        readOnly
+      />
+      <div className="container-fluid p-0 mt-2 ">
+        <button type="submit" className="btn btn-primary w-100 fw-bold ">
+          Registra alimento
+        </button>
+      </div>
+    </Form>
+  );
 }
