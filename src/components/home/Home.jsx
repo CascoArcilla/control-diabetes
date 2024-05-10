b53cec4a09d8b396d00b1b5e3f08d744278ad81c
CascoArcilla <normon1230@gmail.com>
2024-05-09 22:10:44 -0600

    Se agrega grafica en home para mostrar registros de glucosa

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import { useAuth } from "../../auth/AuthPorvider";
 import { useHome } from "../../hooks/useHome.jsx";
 
-import HistorialGlucosa from "../graficos/HistorialGlucosa";
+import HistorialGlucosa from "./HistorialGlucosa.jsx";
 import DetailToday from "./DetailToday";
 
 export default function Home() {
@@ -9,7 +9,7 @@ export default function Home() {
   const { todadayRegisters, lastReisterGlucosa } = useHome();
 
   return (
-    <div className="container-sm d-flex flex-column ">
+    <section className="container-sm d-flex flex-column flex-grow-1 p-0">
       <h1 className="text-center">
         Bienvenido{" "}
         <span className="text-capitalize ">{user.nombre ?? "No name"}</span>
@@ -17,6 +17,6 @@ export default function Home() {
       <h3 className="text-center mt-3 fw-bold ">Tus metricas de hoy</h3>
       <DetailToday registersToday={lastReisterGlucosa} />
       <HistorialGlucosa todayGlucosa={todadayRegisters} />
-    </div>
+    </section>
   );
 }
